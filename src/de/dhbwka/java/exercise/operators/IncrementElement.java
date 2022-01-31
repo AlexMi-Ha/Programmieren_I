f40254142f71d14fcd52ddd228cf594fd1c29425
Alex Hagl <alex@hagl.de>
2022-01-31 18:08:45 +0100

    Comments

--- src/de/dhbwka/java/exercise/operators/IncrementElement.java
+++ src/de/dhbwka/java/exercise/operators/IncrementElement.java
@@ -3,19 +3,19 @@ package de.dhbwka.java.exercise.operators;
 public class IncrementElement {
 
 	public static void main(String[] args) {
-		int i=0;
-		int j=0;
-		j = ++i;
-		int k = j++ + ++i;
-		System.out.println("k: " + k);
-		System.out.println("*: " + j++ + ++i);
-		System.out.println(j++ + ++i);
-		int m = j++ * ++i;
-		System.out.println("m: " + m);
-		int n = --j * --i;
-		System.out.println("n: " + n);
-		System.out.println("i: " + i);
-		System.out.println("j: " + j);
+		int i=0;	// i: 0 j:
+		int j=0;	// i: 0 j: 0
+		j = ++i; // i , j = 1
+		int k = j++ + ++i; // k = 3, j,i = 2
+		System.out.println("k: " + k); // k = 3
+		System.out.println("*: " + j++ + ++i); // j,i = 3  / Ausgabe 23
+		System.out.println(j++ + ++i); // j,i = 4 / Ausgabe 7
+		int m = j++ * ++i; // m = 20, i,j = 5
+		System.out.println("m: " + m); // m: 20
+		int n = --j * --i; // n = 16, j,i = 4
+		System.out.println("n: " + n); // 16
+		System.out.println("i: " + i); // 4
+		System.out.println("j: " + j); // 4
 	}
 
 }
--- src/de/dhbwka/java/exercise/operators/Priority.java
+++ src/de/dhbwka/java/exercise/operators/Priority.java
@@ -3,21 +3,21 @@ package de.dhbwka.java.exercise.operators;
 public class Priority {
 
 	public static void main(String[] args) {
-		System.out.println("1: " + (5 / 2 * 2));
-		System.out.println("2: " + (9. / 2 * 5));
+		System.out.println("1: " + (5 / 2 * 2)); // 4
+		System.out.println("2: " + (9. / 2 * 5)); // 22.5
 		boolean a = true, b = false, c = false;
-		System.out.println("3: " + (a && b || c));
+		System.out.println("3: " + (a && b || c)); // false
 		char ch = 'c';
-		System.out.println("4: " + ('a' + 1 < ch));
+		System.out.println("4: " + ('a' + 1 < ch)); // true
 		int i = 1, j = 2, k = 3;
-		System.out.println("5: " + (-i - 5 * j >= k + 1));
+		System.out.println("5: " + (-i - 5 * j >= k + 1)); //false
 		i = 1;
-		if (a || (++i == 2)) {
-		System.out.println("6: " + i);
+		if (a || (++i == 2)) { // verkürztes oder // zweiter Operand wird nicht mehr ausgeführt
+			System.out.println("6: " + i); // 1
 		}
 		i = 1;
 		if (a | (++i == 2)) {
-		System.out.println("7: " + i);
+			System.out.println("7: " + i); // 2
 		}
 
 
